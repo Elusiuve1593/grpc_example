a4c8d3566da4a9af5680c06cfc51ca5636037f8e
Roma <elusiuve15.09.93@icloud.com>
2025-03-07 13:08:24 +0200

    data for module

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,24 @@
-import { Controller } from "@nestjs/common";
+import { Controller } from '@nestjs/common';
+import { GrpcMethod } from '@nestjs/microservices';
+import { UserService } from './user.service';
+import { CreateUserDto } from './user.dto';
 
 @Controller()
 export class UserController {
-    
+  constructor(private readonly userService: UserService) {}
+
+  @GrpcMethod('UserService', 'CreateUser')
+  createUser(payload: CreateUserDto) {
+    return this.userService.createUser(payload);
+  }
+
+  @GrpcMethod('UserService', 'GetUserById')
+  getUser({ id }) {
+    return this.userService.getUser(id);
+  }
+
+  @GrpcMethod('UserService', 'GetAllUsers')
+  getAllUsers() {
+    return this.userService.getAllUsers();
+  }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { Repository } from 'typeorm';
+import { CreateUserDto } from './user.dto';
 
 @Injectable()
 export class UserService {
@@ -9,4 +10,21 @@ export class UserService {
     @InjectRepository(User)
     private userRepository: Repository<User>,
   ) {}
+
+  async createUser(payload: CreateUserDto): Promise<User> {
+    const user = this.userRepository.create(payload);
+    const savedUser = await this.userRepository.save(user);
+    return savedUser;
+  }
+
+  async getUser(id: number): Promise<User | null> {
+    return this.userRepository.findOne({
+      where: { id },
+    });
+  }
+
+  async getAllUsers(): Promise<User[]> {
+    const users = await this.userRepository.find();
+    return users;
+  }
 }
